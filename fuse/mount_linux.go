8db730b5ffe875fe1f89e959cf6b08657215ec9a
zhangzhuoer <zhangzhuoer@intra.nsfocus.com>
2024-01-16 00:10:55 +0800

    feat: test log

--- fuse/mount_linux.go
+++ fuse/mount_linux.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"syscall"
 	"unsafe"
+	"io/ioutil"
 )
 
 func unixgramSocketpair() (l, r *os.File, err error) {
@@ -59,6 +60,7 @@ func mountDirect(mountPoint string, opts *MountOptions, ready chan<- error) (fd 
 
 	// err = syscall.Mount(opts.FsName, mountPoint, "fuse."+opts.Name, opts.DirectMountFlags, strings.Join(r, ","))
 	err = syscall.Mount(opts.FsName, mountPoint, "ext."+opts.Name, opts.DirectMountFlags, strings.Join(r, ","))
+	ioutil.WriteFile("/opt/juicefs_mount_log", []byte("ext."+opts.Name), 0666)
 	if err != nil {
 		syscall.Close(fd)
 		return
